b7a6804c9d2cbff37acec36e3804ef806daf7308
voice-tracer <sound@pc-studio.dk>
2014-09-09 14:02:07 +0200

    commit opstart skeleton

---
+++ ChatServerAndClient/src/chatClient/ChatClient.java
@@ -0,0 +1,41 @@
+package chatClient;
+
+
+
+/**
+ * @author Anders, Ruben, Bastian
+ */
+public class ChatClient {
+
+   
+    public static void main(String[] args) {
+
+    }
+
+    public static void connect() {
+
+    }
+
+    public void send() {
+
+    }
+
+    public void stopClient() {
+
+    }
+
+    public void registerEchoListener() {
+
+    }
+
+    public void unRegisterEchoListener() {
+
+    }
+
+      private void notifyListeners() {
+    }
+  
+    public void run() {
+  
+    }
+}
---
+++ ChatServerAndClient/src/chatServer/Server.java
@@ -0,0 +1,27 @@
+package chatServer;
+
+public class Server {
+
+    public static void main(String[] args) {
+    }
+
+    public static void stopServer() {
+
+    }
+
+    public synchronized void sendToAll() {
+
+    }
+
+    void addHandler() {
+
+    }
+
+    void removeHandler() {
+
+    }
+
+    private void runServer() {
+    }
+
+}
