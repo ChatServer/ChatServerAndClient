a64f02a9205d7f15cf411983e0458a0cd9bd24bd
voice-tracer <sound@pc-studio.dk>
2014-09-11 14:06:51 +0200

    Gruppe commit - ændringe i clienthandler server forhold

--- ChatServerAndClient/src/chatClient/ChatClient.java
+++ ChatServerAndClient/src/chatClient/ChatClient.java
@@ -1,57 +1,87 @@
 package chatClient;
 
+import chatServer.ChatServer;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
-
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Anders, Ruben, Bastian
  */
-public class ChatClient extends Thread{
+public class ChatClient implements Runnable {
+
     private Scanner input;
     private PrintWriter output;
     Socket socket;
-    private InetAddress serverAddress;    
+    private InetAddress serverAddress;
     private int port;
-   
-    public static void main(String[] args) {
 
+    public ChatClient(InetAddress serverAddress, int port) {
+        this.serverAddress = serverAddress;
+        this.port = port;
     }
 
-    public void connect(String address, int port) throws UnknownHostException, IOException{
+    public void connect(String address, int port) throws UnknownHostException, IOException {
         this.port = port;
         serverAddress = InetAddress.getByName(address);
         socket = new Socket(serverAddress, port);
         input = new Scanner(socket.getInputStream());
         output = new PrintWriter(socket.getOutputStream(), true);
-    }
-    
 
-    public void send() {
+    }
+
+    public void sendConnect(String name) {
+        try {
+            String protocol = "CONNECT#" + name; //IMPORTANT blocking call
+            send(protocol);
+        } catch (IOException ex) {
+            Logger.getLogger(ChatClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void sendmessage(String name, String message) {
+        try {
+            /* nameList split ","*/
+
+            String protocol = "SEND#" + name + "#" + message; //IMPORTANT blocking call
+            send(protocol);
+        } catch (IOException ex) {
+            Logger.getLogger(ChatClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void sendClose() {
+        try {
+            String protocol = "CLOSE#"; //IMPORTANT blocking cal
+            send(protocol);
+        } catch (IOException ex) {
+            Logger.getLogger(ChatClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void send(String protocol) throws IOException {
+        if (socket.isOutputShutdown()) {
+            throw new IOException("Outbound socket is closed");
+        }
+        output.println(protocol);
+    }
+
+    public void run() {
+        String msg;
+        while (true) {
+            msg = input.nextLine();
+            System.out.println("Got the protocol: " + msg);
+        }
+         /* incomming msg protocol split ","*/
+        
+        
         
     }
-
-    public void stopClient() {
-
-    }
-
-    public void registerEchoListener() {
-
-    }
-
-    public void unRegisterEchoListener() {
-
-    }
-
-      private void notifyListeners() {
-    }
-  
-    public void run() {
-  
-    }
+    
 }
